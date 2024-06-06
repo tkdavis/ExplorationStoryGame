9f440e919ed9c0816d421d235f9a6d4d61693b8e
Tyler Davis <tylerkoreydavis@gmail.com>
2024-06-05 18:56:31 -0700

    begin water mesh generation

---
+++ Assets/Features/Water/WaterGenerator.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(MeshFilter))]
+public class WaterGenerator : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+    private void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
+    }
+
+    private void CreateShape()
+    {
+        vertices = new Vector3[]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(0, 0, 1),
+            new Vector3(1, 0, 0),
+            new Vector3(1, 0, 1),
+        };
+
+        triangles = new int[]
+        {
+            0, 1, 2,
+            1, 3, 2
+        };
+    }
+
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+}
