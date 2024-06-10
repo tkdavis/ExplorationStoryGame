ac3f3bd24724036b2efefb0e017513f9ca40eb49
Tyler Davis <tylerkoreydavis@gmail.com>
2024-06-10 13:40:44 -0700

    generate water dynamically by resolution

--- Assets/Features/Water/WaterGenerator.cs
+++ Assets/Features/Water/WaterGenerator.cs
@@ -5,8 +5,10 @@ using UnityEngine.UIElements;
 [RequireComponent(typeof(MeshFilter))]
 public class WaterGenerator : MonoBehaviour
 {
+    public int resolution = 4;
     Mesh mesh;
     Vector3[] vertices;
+
     int[] triangles;
     private void Start()
     {
@@ -19,19 +21,36 @@ public class WaterGenerator : MonoBehaviour
 
     private void CreateShape()
     {
-        vertices = new Vector3[]
-        {
-            new Vector3(0, 0, 0),
-            new Vector3(0, 0, 1),
-            new Vector3(1, 0, 0),
-            new Vector3(1, 0, 1),
-        };
+        vertices = new Vector3[(resolution+1) * (resolution+1)];
 
-        triangles = new int[]
+        for (int y = 0; y <= resolution; y++)
         {
-            0, 1, 2,
-            1, 3, 2
-        };
+            for (int x = 0; x <= resolution; x++)
+            {
+                vertices[y * (resolution + 1) + x] = new Vector3(x, 0, y);
+            }
+        }
+
+        triangles = new int[resolution * resolution * 6];
+
+        int tindex = 0;
+        for (int y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                int vertIndex = y * (resolution + 1) + x;
+
+                triangles[tindex] = vertIndex;
+                triangles[tindex + 1] = vertIndex + resolution + 1;
+                triangles[tindex + 2] = vertIndex + 1;
+
+                triangles[tindex + 3] = vertIndex + 1;
+                triangles[tindex + 4] = vertIndex + resolution + 1;
+                triangles[tindex + 5] = vertIndex + resolution + 2;
+
+                tindex += 6;
+            }
+        }
     }
 
     private void UpdateMesh()
